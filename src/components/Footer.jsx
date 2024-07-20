3d6a623019434e00a717d74139bb1aa9598972ce
Денис <161925751+DenisNysheta@users.noreply.github.com>
2024-07-20 14:31:23 +0300

    fix icons socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "../assets/styles/_App.scss"
-import svg from "../assets/sprite.svg"
 
 
 function Footer() {
@@ -11,23 +10,14 @@ function Footer() {
         <ul className='footer__socials'>
             <li className='footer__link'>
                 <a aria-label='instagram' target='_blank' href="https://www.instagram.com/marvel/">
-                    <svg>
-                        <use xlinkHref={`${svg}#instagram`}></use>
-                    </svg>
                 </a>
             </li>
             <li className='footer__link'>
                 <a aria-label='facebook' target='_blank' href="https://www.facebook.com/MarvelAUNZ/events/?locale=uk_UA&_rdr">
-                    <svg>
-                        <use xlinkHref={`${svg}#facebook`}></use>
-                    </svg>
                 </a>
             </li>
             <li className='footer__link'>
                 <a aria-label='twitter' target='_blank' href="https://x.com/Marvel?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-                    <svg>
-                        <use xlinkHref={`${svg}#twitter-icon`}></use>
-                    </svg>
                 </a>
             </li>
         </ul>
